47aa87086d26619d98eee96803a9f67f51a63c67
ahmedsakr <ahmad.sakr1997@hotmail.com>
2018-07-28 09:09:37 -0700

    Added basic bootstrap template

---
+++ app.js
@@ -0,0 +1,15 @@
+// Imports for needed modules
+var express = require('express')
+var path = require('path')
+
+// Create the app and import css stylesheets
+var app = express()
+app.use(express.static(__dirname + '/ui/style'));
+
+
+// Send the default webpage to the user
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname + '/ui/index.html'));
+})
+
+app.listen(3000);
